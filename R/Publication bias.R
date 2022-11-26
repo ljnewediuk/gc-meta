c76a846635b4509f47645a4e43d9ddb7ecf4be39
drbath <99725383+drbath@users.noreply.github.com>
2022-11-25 20:59:27 -0330

    Publication bias and barchart code

--- R/Publication bias.R
+++ R/Publication bias.R
@@ -1,6 +1,8 @@
+library(metafor)
+
 #### Rosenberg FSN ####
 #Check for publication bias with Rosenber Fail-safe  Numner
-#Remember, if FSN >5k+10 (in this case, >225) then publication bias unlikely
+#Remember, if FSN >5k+10 then publication bias unlikely
 #Fitness, FSN= 2855, target: FSN>615
 Fitnessfsnstudy<-fsn(yi_f, vi_f, data=dat_es, type="Rosenberg", digits=4)
 Fitnessfsnstudy
@@ -10,13 +12,67 @@ GCfsnstudy<-fsn(yi_gc, vi_gc, data=dat_es, type="Rosenberg", digits=4)
 GCfsnstudy
 
 #### Funnel plots for moderators ####
+
 #Fitness funnel plot
 f_mod <- rma.mv(data = dat_es, yi = yi_f, V = vi_f, 
                 random = list(~ 1|study_id/uid, ~1|species_name, ~1|fitness_type))
 funnel(f_mod, level=c(90, 95, 99), shade=c("white", "#FFC107", "#0097A7"), refline=0, legend=TRUE)
+
 #GCs funnel plot
 gc_mod <- rma.mv(data = dat_es, yi = yi_gc, V = vi_gc, random = list(~ 1|study_id/uid, ~1|species_name, ~1|stressor_type))
 funnel(gc_mod, level=c(90, 95, 99), shade=c("white", "#FFC107", "#0097A7"), refline=0, legend=TRUE)
-                 
-#### Subset analysis ####
+
+# Sex moderator on GCs
+sex_gc_mod <- rma.mv(data = dat_es, yi = yi_gc, V = vi_gc, 
+       mods = ~ factor(sex), 
+       random = list(~ 1|study_id/uid, ~1|species_name, ~1|stressor_type))
+funnel(sex_gc_mod, level=c(90, 95, 99), shade=c("white", "#FFC107", "#0097A7"), refline=0, legend=TRUE)
+
+# Sex moderator on fitness
+sex_fit_mod <- rma.mv(data = dat_es, yi = yi_f, V = vi_f,
+       mods = ~ factor(sex), 
+       random = list(~ 1|study_id/uid, ~1|species_name, ~1|fitness_type))
+funnel(sex_fit_mod, level=c(90, 95, 99), shade=c("white", "#FFC107", "#0097A7"), refline=0, legend=TRUE)
+
+# longevity moderator on GCs
+lon_gc_mod <- rma.mv(data = dat_es, yi = yi_gc, V = vi_gc, 
+       mods = ~ lifespan, 
+       random = list(~ 1|study_id/uid, ~1|species_name, ~1|stressor_type))
+funnel(lon_gc_mod, level=c(90, 95, 99), shade=c("white", "#FFC107", "#0097A7"), refline=0, legend=TRUE)
+
+# longevity moderator on fitness
+lon_fit_mod <- rma.mv(data = dat_es, yi = yi_f, V = vi_f, 
+       mods = ~ lifespan, 
+       random = list(~ 1|study_id/uid, ~1|species_name, ~1|fitness_type))
+funnel(lon_fit_mod, level=c(90, 95, 99), shade=c("white", "#FFC107", "#0097A7"), refline=0, legend=TRUE)
+
+# LHS moderator on fitness
+LHS_fit_mod <- rma.mv(data = dat_es, yi = yi_gc, V = vi_gc, 
+       mods = ~ factor(life_history_stage), 
+       random = list(~ 1|study_id/uid, ~1|species_name, ~1|stressor_type))
+funnel(LHS_fit_mod, level=c(90, 95, 99), shade=c("white", "#FFC107", "#0097A7"), refline=0, legend=TRUE)
+
+# LHS moderator on GCs
+LHS_gc_mod <- rma.mv(data = dat_es, yi = yi_f, V = vi_f, 
+       mods = ~ factor(life_history_stage), 
+       random = list(~ 1|study_id/uid, ~1|species_name, ~1|fitness_type))
+funnel(LHS_gc_mod, level=c(90, 95, 99), shade=c("white", "#FFC107", "#0097A7"), refline=0, legend=TRUE)
+
+
+#### Publication bias models ####
+
+#Bivariate model for fitness publication bias
+fit_pb_test <- rma.mv(data = dat_es, yi = yi_f, V = vi_f, mods = I(1/n_fitness_low),
+                        random = list(~ 1|study_id/uid, ~1|species_name, ~1|fitness_type))
+
+#Bivariate model for GCs publication bias
+GC_pb_test <- rma.mv(data = dat_es, yi = yi_gc, V = vi_gc, mods = I(1/n_fitness_low),
+                        random = list(~ 1|study_id/uid, ~1|species_name, ~1|fitness_type))
+
+
+
+
+
+
+
 
---
+++ R/sublit-Barchart code.R
@@ -0,0 +1,45 @@
+#loading in neccesary packages#
+require(tidyverse)
+require(plyr)
+require(ggplot2)
+
+# Import data set #
+library(readxl)
+GC_sublit_data <- read_excel("Desktop/GC_Analysis_sublit/GC_sublit_data.xlsx")
+View(GC_sublit_data)
+#rename#
+sublit <- GC_sublit_data
+
+#Organizning data#
+sublit %>% 
+  #Filtering for usable papers#
+  filter(usable=="y") %>%
+  #summarising number of papers under each conditions (4 in total)#
+  ddply(.(`Measured fitness`, `Fitness implications of GCs`), summarise, value=sum(value)) %>%
+  #combining columns: 'Measured fitness' and 'fitness implications of GC's' for creating plot#
+  unite(., FM_and_I, c(`Measured fitness`, `Fitness implications of GCs`), sep='&') %>%
+  
+  ddply(.(FM_and_I), summarise, ratio=value/35, percent=round((value/35)*100, digits = 0)) %>%
+ mutate(perc=case_when(percent>0 ~ "%")) %>%
+unite(., percent, c('percent', 'perc'), sep="") %>%
+  #plot for barchart#
+  ggplot(aes(x=factor(FM_and_I, levels=c("n&y", "y&y", "y&n", "n&n"), labels = c("Did not measure fitness\nmade implication\non population health", "Measured fitness\nmade implication\non population health", "Measured fitness\ndid not make implication\non population health", "Did not measure fitness\ndid not make implication\non population health")), y=ratio*100, fill=FM_and_I)) +
+  geom_col(color="black") +
+  geom_text(aes(label = percent), colour="white",  
+            position = position_stack(vjust = 0.5)) +
+  scale_fill_brewer(palette = "BrBG", guide=NULL) +
+  labs(x=NULL, y="Percentage of papers (%)") +
+  scale_y_continuous(limits=c(0,60), breaks=seq(0, 60, 15)) +
+  theme(panel.border = element_rect(colour = "black", size=1, fill=FALSE),
+    panel.background = element_rect(fill = "white"), 
+    axis.title.y = element_text(size = 15), 
+    axis.text = element_text(size=13))
+
+# Save plot
+ggsave('figures/bar_chart.tiff', 
+       last_plot(), device = 'tiff', 
+       width = 32, height = 12, units = 'cm', dpi = 300)
+
+
+
+
